8e9e971ec3182dba0736cf2ebd17f561156e0026
Slawomir <slawomir.chochorek@salesmanago.pl>
2018-05-29 10:17:00 +0200

    added ip method to service

--- src/Services/CreateAppStoreAccountService.php
+++ src/Services/CreateAppStoreAccountService.php
@@ -159,4 +159,21 @@ class CreateAppStoreAccountService extends BasicAccountService
         }
     }
 
+    public function getContactIp() {
+        if (getenv('HTTP_CLIENT_IP'))
+            $ip = getenv('HTTP_CLIENT_IP');
+        else if(getenv('HTTP_X_FORWARDED_FOR'))
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+        else if(getenv('HTTP_X_FORWARDED'))
+            $ip = getenv('HTTP_X_FORWARDED');
+        else if(getenv('HTTP_FORWARDED_FOR'))
+            $ip = getenv('HTTP_FORWARDED_FOR');
+        else if(getenv('HTTP_FORWARDED'))
+            $ip = getenv('HTTP_FORWARDED');
+        else
+            $ip = getenv('REMOTE_ADDR');
+
+        return $ip;
+    }
+
 }
