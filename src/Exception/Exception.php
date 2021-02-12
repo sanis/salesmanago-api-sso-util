7002f40f569d2a5925add3a4710cadefe1e92b3a
Ruslan <ruslan.barlozhetskyi@salesmanago.pl>
2021-02-12 10:49:58 +0100

    add Exception structures;

--- src/Exception/Exception.php
+++ src/Exception/Exception.php
@@ -5,16 +5,26 @@ namespace SALESmanago\Exception;
 
 class Exception extends \Exception
 {
+    const
+        EXCEPTION_HEADER_NAME = 'SALESmanago: ' . PHP_EOL,
+        TRACE = 'Trace: ' . PHP_EOL,
+        MESSAGE = 'Message: ',
+        FILE = 'File: ',
+        LINE = 'Line: ';
 
     /**
      * @return string - massage for logs files
      */
     public function getLogMessage()
     {
-        /**
-         * @TODO change this one
-         */
-        return $this->getMessage();
+        $message = self::EXCEPTION_HEADER_NAME;
+        $message.= self::MESSAGE;
+        $message.= $this->getMessage() . PHP_EOL;
+        $message.= self::FILE . $this->getFile() . PHP_EOL;
+        $message.= self::LINE . $this->getLine() . PHP_EOL;
+        $message.= self::TRACE;
+        $message.= $this->getTraceAsString() . PHP_EOL;
+        return $message;
     }
 
     /**
@@ -22,20 +32,9 @@ class Exception extends \Exception
      */
     public function getViewMessage()
     {
-        /**
-         * @TODO change this one
-         */
-        return $this->getMessage();
-    }
-
-    /**
-     * @return string - massage for browser console.
-     */
-    public function getConsoleMessage()
-    {
-        /**
-         * @TODO change this one
-         */
-        return $this->getMessage();
+        $message = $this->getMessage() . ': ';
+        $message.= $this->getFile() . ': ';
+        $message.= $this->getLine() . PHP_EOL;
+        return $message;
     }
 }
