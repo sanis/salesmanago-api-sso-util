670f33d46559e04595fe383cfb9503e31f6a13d7
Ruslan <ruslan.barlozhetskyi@salesmanago.pl>
2021-04-08 12:24:57 +0200

    fix HealthModel empty line

--- src/Model/Report/HealthModel.php
+++ src/Model/Report/HealthModel.php
@@ -5,7 +5,9 @@ namespace SALESmanago\Model\Report;
 
 class HealthModel extends AbstractBasicReportModel
 {
-
+    /**
+     * @return array|mixed
+     */
     public function getDataToSend()
     {
         return array_merge($this->getBasicReportData(), ['error' => $this->rawData]);
