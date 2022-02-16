ee478b1fc51efeced6cde0e59b4c2bd42e3b79f5
Ruslan <ruslan.barlozhetskyi@salesmanago.com>
2022-02-16 19:53:47 +0100

    test

--- tests/Unit/TestCaseUnit.php
+++ tests/Unit/TestCaseUnit.php
@@ -21,6 +21,10 @@ class TestCaseUnit extends TestCase
         $userEmail = getenv('userEmail');
         $userPass = getenv('userPass');
 
+        var_dump($userEmail);
+        var_dump($userPass);
+        die('test 321');
+
         $conf = Configuration::getInstance();
         $user = new User();
 
